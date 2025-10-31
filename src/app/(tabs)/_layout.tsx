2b2d38e0d00bf38d99e684748f62c7603718bca8
Yggdrae <victorcnicacio@hotmail.com>
2025-10-30 21:58:27 -0300

    feat: tabs layout

---
+++ src/app/(tabs)/_layout.tsx
@@ -0,0 +1,40 @@
+import Home from './home';
+import { useState } from 'react';
+import Settings from './settings';
+import { BottomNavigation } from 'react-native-paper';
+
+const HomeRoute = () => <Home />;
+const SettingsRoute = () => <Settings />;
+
+const MyComponent = () => {
+  const [index, setIndex] = useState(0);
+  const [routes] = useState([
+    {
+      key: "home",
+      title: "Home",
+      focusedIcon: "home",
+      unfocusedIcon: "home-outline",
+    },
+    {
+      key: "settings",
+      title: "Settings",
+      focusedIcon: "cog",
+      unfocusedIcon: "cog-outline",
+    },
+  ]);
+
+  const renderScene = BottomNavigation.SceneMap({
+    home: HomeRoute,
+    settings: SettingsRoute,
+  });
+
+  return (
+    <BottomNavigation
+      navigationState={{ index, routes }}
+      onIndexChange={setIndex}
+      renderScene={renderScene}
+    />
+  )
+}
+
+export default MyComponent;
