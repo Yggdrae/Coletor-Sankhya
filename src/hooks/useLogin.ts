a3576b109a61271b8f1c00eff45e185758101bad
Yggdrae <victorcnicacio@hotmail.com>
2025-11-12 00:20:36 -0300

    feat: save username and token on context from login data

--- src/hooks/useLogin.ts
+++ src/hooks/useLogin.ts
@@ -1,14 +1,19 @@
 import { useMutation } from "@tanstack/react-query";
 import { loginApi } from "@/src/service/auth";
-import { LoginDTO } from "../interface/ILogin";
+import { LoginDTO, LoginResponse } from "../interface/ILogin";
+import { useUser } from "../context/userContext";
 
 export const useLogin = () => {
+  const { setUserName, setAccessToken } = useUser();
+
   return useMutation({
-    mutationFn: async (loginData: LoginDTO) => {
-      await loginApi(loginData);
+    mutationFn: async (loginData: LoginDTO): Promise<LoginResponse> => {
+      return await loginApi(loginData);
     },
     onSuccess: (data) => {
       console.log("Login bem-sucedido!", data);
+      setUserName(data.usuario.nome)
+      setAccessToken(data.access_token)
     },
     onError: (error) => {
       console.error("Erro no login:", error.message);
